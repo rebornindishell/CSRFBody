e36bc98ac496f98780ca5774c6153b87649e1cd1
rebornindishell <adityapadhya@gmail.com>
2021-06-22 11:07:18 +0400

    Update CSRFbody.py

--- CSRFbody.py
+++ CSRFbody.py
@@ -19,16 +19,21 @@ csrf=csrflist[5]
 captcha=list[7]
 print(captcha)
 print(csrf)
-burp0_cookies = {"JSESSIONID": "C3BDB9A73845E19E87C2B2DFC0A2E8F6"}
-burp0_headers = {"Cache-Control": "max-age=0", 
-"Upgrade-Insecure-Requests": "1", 
-"Origin": "http://origin", 
-"Content-Type": "application/x-www-form-urlencoded",
-"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36", 
-"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9","Referer": "http://refererurl", 
-"Accept-Encoding": "gzip, deflate", 
-"Accept-Language": "en-US,en;q=0.9", 
-"Connection": "close"}
+burp0_cookies = {
+			"JSESSIONID": "C3BDB9A73845E19E87C2B2DFC0A2E8F6"
+		}
+burp0_headers = {
+			"Cache-Control": "max-age=0", 
+			"Upgrade-Insecure-Requests": "1", 
+			"Origin": "http://origin", 
+			"Content-Type": "application/x-www-form-urlencoded",
+			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36", 
+			"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+			"Referer": "http://refererurl", 
+			"Accept-Encoding": "gzip, deflate", 
+			"Accept-Language": "en-US,en;q=0.9", 
+			"Connection": "close"
+}
 url1 = 'http://loginurl/post'
 values = {
 			'username': 'username',
